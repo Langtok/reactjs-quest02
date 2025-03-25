219fafde998e8b17b928dff4ea4cfbbff2413184
Langtock <70757120+Langtok@users.noreply.github.com>
2025-03-25 11:08:14 +0100

    Ex01 Generated

---
+++ ex01/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div>
+      My First Component!
+      <p className="lorem_ipsum">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu dui vitae purus faucibus rhoncus vel vel purus. Duis convallis nisi nec purus facilisis euismod a non ligula. Fusce cursus nunc eget velit egestas, ac vehicula risus pharetra. Aenean dictum felis nec maximus faucibus. In feugiat sem sed massa tincidunt pellentesque. Integer a ipsum nisi. Nunc mauris purus, ornare nec magna et, varius fermentum enim. Morbi in dui pellentesque, scelerisque est condimentum, sodales sapien. Aliquam erat metus, interdum et ligula in, sodales vestibulum eros. Cras id dolor ac lacus convallis posuere. Curabitur sit amet efficitur augue, non dapibus neque.</p>
+
+      </div>
+
+    </div>
+    
+  );
+}
+
+export default App;
+
+      
+
+
